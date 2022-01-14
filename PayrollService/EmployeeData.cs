9d6e1bb6f3a44c2666948c6b516a7a6caa1be9c9
“ritwik012” <rutwikgulhaneg@gmail.com>
2022-01-15 00:46:18 +0530

    [Rutwik]: Added Update Data File

--- PayrollService/EmployeeData.cs
+++ PayrollService/EmployeeData.cs
@@ -7,10 +7,9 @@ namespace Payroll_Service
     public class EmployeeData
 
     {
-        public int id { get; set; }
         public string Name { get; set; }
-        public float Basic_pay { get; set; }
-        public DateTime Date { get; set; }
+        public double Basic_pay { get; set; }
+        public DateTime StartDate { get; set; }
         public char gender { get; set; }
         public string phone { get; set; }
         public string Address { get; set; }
@@ -19,5 +18,6 @@ namespace Payroll_Service
         public double Taxable_pay { get; set; }
         public double IncomeTax_pay { get; set; }
         public double Net_Pay { get; set; }
+
     }
 }
--- PayrollService/Program.cs
+++ PayrollService/Program.cs
@@ -6,8 +6,48 @@ namespace Payroll_Service
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book ");
+            bool flag = true;
             EmployeeConfig employeeConfig = new EmployeeConfig();
+            EmployeeData data = new EmployeeData();
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Addition, 2-Delete by ID, 3-Update Employee, 4-View, 5-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:                           // It Will Add Data to the Table
+                        data.Name = "Gyanendra";
+                        data.Basic_pay = 89000.45;
+                        data.StartDate = DateTime.Now;
+                        data.gender = 'M';
+                        data.phone = "7440922090";
+                        data.Address = "Rewa";
+                        data.Department = "HR";
+                        data.Deduction = 567.34;
+                        data.Taxable_pay = 34.23;
+                        data.IncomeTax_pay = 10.23;
+                        data.Net_Pay = 1000;
+                        var result = employeeConfig.AddEmployee(data);
+                        if (result != null)
+                            Console.WriteLine("Data Got added Successfully");
+                        else
+                            Console.WriteLine("Provide Correct Data According to Coloumn");
+                        break;
+                    case 2:                             // It Will Delete Data from the table According to Their Respective ID
+                        Console.WriteLine("Enter the id to Delete Data");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        employeeConfig.DeleteEmployee(num);
+                        break;
+                    case 3:
+                        data.Name = "Gyanendra";        // It Will Update Basic Pay Where Name is Gyanendra
+                        data.Basic_pay = 7000.45;
+                        employeeConfig.UpdateEmployee(data);
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
